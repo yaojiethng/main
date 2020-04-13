dfc9bde07f4ee2a70078895654f3120360ac1578
alam8064 <59945273+alam8064@users.noreply.github.com>
2020-04-13 20:54:47 +0800

    fixed travis

--- src/test/java/seedu/jelphabot/model/calendar/CalendarDateTest.java
+++ src/test/java/seedu/jelphabot/model/calendar/CalendarDateTest.java
@@ -1,8 +1,8 @@
 package seedu.jelphabot.model.calendar;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
